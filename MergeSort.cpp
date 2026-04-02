b57245bf6e7f0269510cb61a6d8e971420fbc68f
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-04-02 18:03:38 +0700

    4

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -22,3 +22,7 @@ void input()
             cout << "\nMaksimal panjang array adalah 20";
         }
     }
+
+    cout << "\n-------------------------------" << endl;
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "-------------------------------" << endl;
